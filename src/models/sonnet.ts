c58f18da23631fb5c2415a3e4e9ab86cdd6a8da8
Thomas <thomas@thomaswilson.xyz>
2025-11-30 09:25:59 +0000

    refactor(Sonnet): Have the Sonnet generate its individual Lines

--- src/models/sonnet.ts
+++ src/models/sonnet.ts
@@ -1,23 +1,27 @@
 import { SonnetLine } from './sonnetLine';
 
 export class Sonnet {
+	public readonly lines: SonnetLine[];
+
 	constructor(
 		public readonly id: string,
 		public readonly name: string,
 		private readonly theText: string
-	) {}
+	) {
+		this.lines = theText.split(`\n`).map((text) => new SonnetLine(text));
+	}
 
 	get fullText() {
 		return this.theText;
 	}
 
 	getLine(theLineIndex: number): SonnetLine {
-		const proposedValue = this.theText.split(`\n`)[theLineIndex];
+		const proposedValue = this.lines[theLineIndex];
 
 		if (proposedValue === undefined) {
 			throw new Error(`Line index ${theLineIndex} requested for sonnet, but not found`);
 		}
 
-		return new SonnetLine(proposedValue);
+		return proposedValue;
 	}
 }
